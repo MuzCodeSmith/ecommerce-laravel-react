b5a749fcf797e3a225801e0d082b5827becb3e65
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-03 12:49:59 +0530

    add index and store brands handler

--- app/Http/Controllers/admin/BrandController.php
+++ app/Http/Controllers/admin/BrandController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BrandController extends Controller
 {
-    //
+    public function index(){
+        $brands = Brand::orderBy('created_at','DESC')->get();
+        return response()->json([
+            'status'=>200,
+            'data'=> $brands
+        ],200);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'data'=> $validator->errors()
+            ],400);
+        }
+        $brand = new Brand();
+        $brand->name=$request->name;
+        $brand->status=$request->status;
+        $brand->save();
+
+        return response()->json([
+            'status'=>200,
+            'data'=> $brand
+        ],200);
+    }
 }
