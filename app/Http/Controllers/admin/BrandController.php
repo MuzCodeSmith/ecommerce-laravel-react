ff26ad91422a5ca94d7974f921d5e382183b6a44
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-03 13:13:23 +0530

    add show and update brands handler

--- app/Http/Controllers/admin/BrandController.php
+++ app/Http/Controllers/admin/BrandController.php
@@ -24,7 +24,7 @@ class BrandController extends Controller
         if($validator->fails()){
             return response()->json([
                 'status'=>400,
-                'data'=> $validator->errors()
+                'message'=> $validator->errors()
             ],400);
         }
         $brand = new Brand();
@@ -37,4 +37,50 @@ class BrandController extends Controller
             'data'=> $brand
         ],200);
     }
+
+    public function show($id){
+        $brand = Brand::find($id);
+        if(!$brand){
+            return response()->json([
+                'status'=>404,
+                'message'=>'Brand not found'
+            ],404);
+        }
+        return response()->json([
+            'status'=>200,
+            'data'=> $brand
+        ],200);
+    }
+
+    public function update($id,Request $request){
+        $brand = Brand::find($id);
+        if(!$brand){
+            return response()->json([
+                'status'=>404,
+                'message'=>"brand not found"
+            ],404);
+        }
+        $validator = Validator::make($request->all(),[
+            "name"=>"required"
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'message'=> $validator->errors()
+            ],400);
+        }
+        $brand->name=$request->name;
+        $brand->status=$request->status;
+        $brand->save();
+
+        return response()->json([
+            'status'=>200,
+            'message'=>"brand updated successfully",
+            'data'=> $brand
+        ],200);
+    }
+
+
+
 }
