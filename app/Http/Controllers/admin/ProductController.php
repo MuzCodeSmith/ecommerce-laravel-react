e00f931fece7e215d6aeec54babd86d494bf8b56
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-04 18:33:13 +0530

    feat(ProductController):implement updated  method for products Added update() to handle product editing via put request. validates incoming data and updates existing product entry

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -60,8 +60,51 @@ class ProductController extends Controller
         
     }
 
-    public function update(){
-        
+    public function update($id, Request $request){
+
+        $product = Product::find($id);
+
+        if(!$product){
+            return response()->json([
+                'status'=>404,
+                'message'=>'Product not found!'
+            ],404);
+        }
+
+        $validator = Validator::make($request->all(),[
+            'title'=>'required',
+            'price'=>'required|numeric',
+            'category'=>'required|integer',
+            'sku'=>'required|unique:products,sku,'.$id.',id',
+            'is_featured'=>'required',
+            'status'=>'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'message'=>$validator->errors()
+            ],400);
+        }
+
+        $product->title = $request->title; 
+        $product->price = $request->price; 
+        $product->compare_price = $request->compare_price; 
+        $product->category_id = $request->category; 
+        $product->brand_id = $request->brand; 
+        $product->sku = $request->sku; 
+        $product->qty = $request->qty; 
+        $product->description = $request->description; 
+        $product->short_description = $request->short_description; 
+        $product->status = $request->status; 
+        $product->barcode = $request->barcode; 
+        $product->is_featured = $request->is_featured; 
+        $product->save();
+
+        return response()->json([
+            'status'=>200,
+            'message'=>'Product has been updated successfully'
+        ],200);
     }
 
     public function destroy(){
