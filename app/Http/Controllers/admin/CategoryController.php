3a3f87b834191bbfb9279ab7e62b8c5b3f60096a
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-04 10:58:15 +0530

    made status required filed for category controller

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -22,6 +22,7 @@ class CategoryController extends Controller
     public function store(Request $request){
         $validator = Validator::make($request->all(),[
             'name'=> 'required',
+            'status'=> 'required',
         ]);
 
         if($validator->fails()){
