1a71d6d21a595e0850bb8c3fe8eb75475157f08a
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-04 17:10:23 +0530

    created api for fetch product sizes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\SizeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,6 @@ Route::post('/admin/login',[AuthController::class,'authenticate']);
 Route::group(['middleware'=>'auth:sanctum'], function(){
     Route::resource('categories',CategoryController::class);
     Route::resource('brands',BrandController::class);
+    Route::get('sizes',[SizeController::class,'index']);
 });
 
