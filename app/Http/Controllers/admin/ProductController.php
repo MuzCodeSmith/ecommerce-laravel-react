c34eab4f6e0c4592e9696ac4808250ee9ca6fecd
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-07 08:23:14 +0530

    refactor(ProductController) implemented logic for uplaod image to existing product

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -185,4 +185,44 @@ class ProductController extends Controller
             'data' => "product has been deleted successfully"
         ], 200);
     }
+
+    public function saveProductImage(Request $request){
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => 400,
+                "errors" => $validator->errors()
+            ], 400);
+        }
+
+        $image = $request->file('image');
+        $imageName = $request->product_id.'-'.time() . '.' . $image->extension();
+        
+        // large Thumbnail
+        $manager = new ImageManager(Driver::class);
+        $img = $manager->read($image->getPathname());
+        $img->scaleDown(1200);
+        $img->save(public_path('uploads/products/large/' . $imageName));
+
+        // small Thumbnail
+        $manager = new ImageManager(Driver::class);
+        $img = $manager->read($image->getPathname());
+        $img->coverDown(400,460);
+        $img->save(public_path('uploads/products/small/' . $imageName));
+
+        // insert record in product images table
+        $productImage = new ProductImage();
+        $productImage->image = $imageName;
+        $productImage->product_id = $request->product_id;
+        $productImage->save();
+        
+        return response()->json([
+            "status" => 200,
+            "message" => "image has been uplaoded successfully!",
+            "data" => $productImage
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::group(['middleware'=>'auth:sanctum'], function(){
     Route::get('sizes',[SizeController::class,'index']);
     Route::resource('products',ProductController::class);
     Route::post('temp-images',[TempImageController::class,'store']);
+    Route::post('save-product-image',[ProductController::class,'saveProductImage']);
 
 });
 
