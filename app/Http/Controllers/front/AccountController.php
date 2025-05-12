04663b7c822963d265fbda6559133ca33b0f58dd
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-12 07:51:27 +0530

    feat(AuthController) create authenticate controller method for user Auth.

--- app/Http/Controllers/front/AccountController.php
+++ app/Http/Controllers/front/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\front;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -37,4 +38,39 @@ class AccountController extends Controller
             'message' => 'you have registered successfully'
         ]);
     }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::user()->id);
+
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'status'=>201,
+                'token'=>$token,
+                'id'=>$user->id,
+                'name'=>$user->name
+            ],200);
+
+        } else {
+            return response()->json([
+                'status' => 401,
+                'errors' => 'Either email/password is incorrect'
+            ], 401);
+        }
+    }
+    
 }
