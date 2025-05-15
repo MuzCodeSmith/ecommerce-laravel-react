1687d550e5a62e1dd93e3e58202d1c265a170ca4
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-05-15 08:39:34 +0530

    feat(admin/OrderController)add new controller methods get orders and single order

---
+++ app/Http/Controllers/admin/OrderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index(){
+        $orders = Order::orderBy('created_at','DESC')->with('items')->get();
+
+        return response()->json([
+            "status"=>200,
+            "data"=>$orders
+        ],200);
+    }
+    public function detail($id){
+        $order = Order::with('items')->find($id);
+
+        return response()->json([
+            "status"=>200,
+            "data"=>$order
+        ],200);
+    }
+}
